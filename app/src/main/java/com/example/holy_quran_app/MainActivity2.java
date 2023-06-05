da5e4a37403f503513d25bcee3cd93a798dc5595
Shoaib Akram <shoaibakram2001@gmail.com>
2023-06-05 23:15:44 +0500

    first initialization

--- app/src/main/java/com/example/holy_quran_app/MainActivity2.java
+++ app/src/main/java/com/example/holy_quran_app/MainActivity2.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Bu24tton;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
